e74429e7f27eee9950a8fa6f4a62bc18e4437f35
HadrienT <tramonihadrien@gmail.com>
2025-01-26 18:21:54 +0000

    (quant-dev) Test : Added a parallel FastAPI server to handle a robots.txt route to block friendly scraping

--- visualization/app.py
+++ visualization/app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from uvicorn import Config, Server
+import threading
 
 st.set_page_config(page_title="Financial Data Experimentation Hub", layout="wide")
+
+app = FastAPI()
+
+
+@app.get("/robots.txt")
+async def robots_txt():
+    return FileResponse("robots.txt")
+
+
+def run_fastapi():
+    config = Config(app=app, host="0.0.0.0", port=8000, log_level="info")
+    server = Server(config)
+    server.run()
+
+
+thread = threading.Thread(target=run_fastapi, daemon=True)
+thread.start()
+
 from about import page_about
 from home_page import page_home
 from impliedVolatility import page_IV
@@ -8,7 +30,6 @@ from pairtrading import page_pairtrading
 from dynamicPortfolio import page_dynamicPortfolio
 from architecture import page_architecture
 
-# --- Navigation bar ---
 st.sidebar.title("Navigation")
 page = st.sidebar.radio(
     "Go to:",
@@ -22,20 +43,15 @@ page = st.sidebar.radio(
     ],
 )
 
-# --- Display the corresponding page ---
 if page == "Home Page":
     page_home()
 elif page == "Make a Portfolio":
     page_dynamicPortfolio()
-
 elif page == "Implied Volatility":
     page_IV()
-
 elif page == "About":
     page_about()
-
 elif page == "Pair Trading":
     page_pairtrading()
-
 elif page == "Architecture":
     page_architecture()
